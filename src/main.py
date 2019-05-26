c7e60e9992839066f45b942bc2762f8b5484edb6
taylor <taylor.radke@outlook.com>
2019-05-26 12:02:12 +1000

    Yen's

--- src/main.py
+++ src/main.py
@@ -8,88 +8,76 @@ def dijkstra(graph, initial, dest):
     dist = {}
     prev = {}
     queue = [(0, initial)]
-    
+
     while queue:
         path_cost, u = heappop(queue)
 
         dist[u] = path_cost
-
-        for v in graph[u]:                
-            alt = graph[u][v] + path_cost
-            if (v in prev and alt < dist[u]) or v not in prev:
-                prev[v] = u
-                heappush(queue, (alt, v))
-
+        if u in graph:
+            for v in graph[u]:                
+                alt = graph[u][v] + path_cost
+                if (v in prev and alt < dist[u]) or v not in prev:
+                    prev[v] = u
+                    heappush(queue, (alt, v))
 
     return dist[dest], shortest_path(prev, initial, dest)
 
 def shortest_path(prev, initial, dest):
-    S = []
+    path = []
     u = dest
     if prev[u] or prev[u] == dest:
         while u:
-            S.insert(0,u)
+            path.insert(0, u)
             if u == initial:
                 break
             u = prev[u]
-    print(S)
-    return S
+    return path
 
 def k_shortest_paths(graph, initial, dest, K):
     dist, path = dijkstra(graph, initial, dest)
-    A = [{"cost": dist, "path": path}]
+
+    dists = [dist]
+    A = [path]
     B = []
-
     
-    # for k in range(1, K):
-    
-    #     for i in range(len(A[k-1]["path"]) - 2):
-    #         edges_removed = []
-    #         spurNode = A[k-1]["path"][i]
-    #         rootPath = A[k-1]["path"][0:i]
-
-    #         for p in A:
-    #             if rootPath == p["path"][0:i]:
-    #                 if p["path"][i] in graph:
-    #                     edges_removed.append({
-    #                         p["path"][i]: graph[p["path"][i]]
-    #                         })
-
-    #                     graph.pop(p["path"][i])
-    #                 if p["path"][i+1] in graph:
-    #                     edges_removed.append(
-    #                         {
-    #                             p["path"][i+1]: graph[p["path"][i+1]]
-    #                         })
-    #                     graph.pop(p["path"][i+1])
-
-    #         for root_path_node in rootPath:
-    #             if root_path_node != spurNode:
-    #                 if root_path_node in graph:
-    #                     edges_removed.append({
-    #                         root_path_node: graph[root_path_node]
-    #                         })
-    #                     graph.pop(root_path_node)
-
-    #         spur_dist, spur_path = dijkstra(graph, spurNode, dest)
-
-    #         total_path = []
-    #         total_path.extend(rootPath)
-    #         total_path.extend(spur_path)
-            
-    #         print(f"i is {i}")
-
-    #         B.append(total_path)
+    for k in range(1, 2):
+        for i in range(len(A[k-1]) - 2):
+          
+            nodes_removed = []
+            vertex_removed = []
+            spur_node = A[k-1][i]
+            root_path = A[k-1][0:i]
         
-    #     if len(B) == 0:
-    #         break
-    #     B.sort()
-    #     A[k] = B[0]
-    #     B.pop()
-    # return A
+            for p in A:
+                if root_path == p[0:i] and p[i] in graph:
+                    vertex_removed.append((p[i], p[i+1], graph[p[i]][p[i+1]]))
+                    graph[p[i]].pop(p[i+1])
+            
+            for root_path_node in root_path:
+                if root_path_node != spur_node:
+                    nodes_removed.append((root_path_node, graph[root_path_node]))
+                    graph.pop(root_path_node)
+            
+            spur_dist, spur_path = dijkstra(graph, spur_node, dest)
+            print(spur_dist)
+            total_path = [item for item in root_path]
+            total_path.extend(spur_path)
+            
+            heappush(B, total_path)
+
+            for ver in vertex_removed:
+                graph[ver[0]][ver[1]] = ver[2]
+
+            for node in nodes_removed:
+                graph[node[0]] = node[1]
+            
+
             
 
 
+    
+            
+        
 def main(input_file):
     with open(input_file) as input:
         n, m = input.readline().replace('\n', '').split(' ')
